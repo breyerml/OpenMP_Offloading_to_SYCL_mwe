0cef19e3af433ce3c24f2142244c50d416b00747
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2021-05-06 15:47:43 +0200

    added data files

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
 
-  simulation sim{ "../data/init_data.out", "../data/compute_data_1.out" };
+  simulation sim{ "../data/init_data.bin", "../data/compute_data_1.bin" };
 
   sim.initialize_arrays();
   sim.compute_monodomain(39);

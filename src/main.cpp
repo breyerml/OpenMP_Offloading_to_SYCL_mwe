4669ac2a813d25cca1fc9c4107414bf18b5a411e
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2021-05-06 16:05:16 +0200

    default: only one repetition

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main() {
   simulation sim{ "../data/init_data.bin", "../data/compute_data_1.bin" };
 
   sim.initialize_arrays();
-  sim.compute_monodomain(39);
+  sim.compute_monodomain();
 
   return 0;
 }

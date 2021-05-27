b76b6150e3ed8bc5b55d5c84386247bca18e23c7
Marcel Breyer <marcel.breyer@ipvs.uni-stuttgart.de>
2021-05-27 13:57:36 +0200

    added .bin files to gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,10 @@
 
 int main() {
 
-  simulation sim{ "../data/init_data.bin", "../data/compute_data_1.bin" };
+  simulation sim{ "../data/init_data_2x2.bin", "../data/compute_data_2x2.bin" };
 
   sim.initialize_arrays();
-  sim.compute_monodomain();
+  sim.compute_monodomain(10);
 
   return 0;
 }
